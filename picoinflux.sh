167ea339c1be742c926d42f3cf2d6f96dfeed207
Benjamin Freitag <benchonaut@yandex.com>
2018-01-23 09:35:32 +0100

    2018-01-23-08-35

--- picoinflux.sh
+++ picoinflux.sh
@@ -13,6 +13,14 @@ hostname=$(cat /etc/picoinfluxid 2>/dev/null || (hostname||(uci show system.@sys
 	echo "udp_connections="$(grep : /proc/1/net/udp|wc -l|cut -d" " -f1)
 	echo "conntrack_connections="$(wc -l /proc/1/net/nf_conntrack|cut -d" " -f1)
 	
+	echo "uptime="$(cut -d" " -f1 /proc/uptime |cut -d. -f1)
+	echo "logdir_size="$(du -m -s /var/log/ 2>/dev/null|cut -d" " -f1)
+	echo "apache_logsize="$(du -m -s /var/log/apache2  2>/dev/null|cut -d" " -f1)
+	echo "mail_log="$(wc -l /var/log/mail.log 2>/dev/null|cut -d " " -f1)
+	echo "mail_err="$(wc -l /var/log/mail.err 2>/dev/null|cut -d " " -f1)
+	echo "mail_warn="$(wc -l /var/log/mail.warn 2>/dev/null|cut -d " " -f1)
+	
+	
 	cat /proc/1/net/wireless |sed 's/ \+/ /g;s/^ //g'|grep :|cut -d" " -f1,4|sed 's/\.//g'|sed 's/^/wireless_level_/g;s/:/=/g;s/ //g'
 	echo "wan_tx_bytes="$(cat /sys/class/net/$(awk '$2 == 00000000 { print $1 }' /proc/net/route)/statistics/tx_bytes)
 	echo "wan_rx_bytes=-"$(cat /sys/class/net/$(awk '$2 == 00000000 { print $1 }' /proc/net/route)/statistics/rx_bytes)
