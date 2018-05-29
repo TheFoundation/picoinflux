c03e573fd52f37142d2b19cce1fed885a3ce00e6
Benjamin Freitag <benchonaut@yandex.com>
2018-05-29 14:47:03 +0200

    2018-05-29-12-47

--- picoinflux.sh
+++ picoinflux.sh
@@ -18,8 +18,9 @@ hostname=$(cat /etc/picoinfluxid 2>/dev/null || (hostname||(uci show system.@sys
 	echo "pingLevel3DNS"$(ping 4.2.2.4 -c 2 -w 2  2>&1|sed 's/.\+time//g' |grep ^=|sort -n|tail -n1|cut -d" " -f1);
 	echo "pingGoogleDNS"$(ping 8.8.8.8 -c 2 -w 2  -c 2 -w 2  2>&1|sed 's/.\+time//g' |grep ^=|sort -n|tail -n1|cut -d" " -f1);
 	c=0;grep ogomip /proc/cpuinfo|while read a;do a=${a// /};echo ${a//:/_$c"="};let c+=1;done |sed 's/ //g'
-	for i in $(seq 0 31);do test -e /sys/devices/system/cpu/cpufreq/policy$i/scaling_cur_freq && echo "cpufreq_"$i"="$(cat /sys/devices/system/cpu/cpufreq/policy2/scaling_cur_freq);done
+	for i in $(seq 0 31);do test -e /sys/devices/system/cpu/cpufreq/policy$i/scaling_cur_freq && echo "cpufreq_"$i"="$(cat /sys/devices/system/cpu/cpufreq/policy$i/scaling_cur_freq);done
 	for i in $(seq 0 31);do test -e /sys/devices/virtual/thermal/thermal_zone$i/temp && echo "temp_"$i"="$(cat /sys/devices/virtual/thermal/thermal_zone$i/temp);done
+	for h in $(seq 0 31);do for i in $(seq 0 31);do test -e /sys/class/hwmon/hwmon$h/device/temp"$i"_input && echo "temp_hwmon_"$h"_"$i"="$(cat /sys/class/hwmon/hwmon$h/device/temp"$i"_input);done;done
 	echo "uptime="$(cut -d" " -f1 /proc/uptime |cut -d. -f1)
 	echo "logdir_size="$(du -m -s /var/log/ 2>/dev/null|cut -d"/" -f1)
 	echo "apache_logsize="$(du -m -s /var/log/apache2  2>/dev/null|cut -d"/" -f1)
