eb7bc514c498c452f651bf156781825ed26df20a
Benjamin Freitag <benchonaut@yandex.com>
2018-06-15 00:31:22 +0200

    2018-06-14-22-31

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 if [ "$(id -u)" == 0 ] ;then
-which git && ( mkdir -p /etc/custom;cd /etc/custom;git clone https://github.com/TheFoundation/picoinflux.git || (cd /etc/custom/picoinflux/ ; git pull ) )  || ( cd /etc/custom/picoinflux/ || ( mkdir /etc/custom/picoinflux/;cd /etc/custom/picoinflux/ ) ;
+which git && ( mkdir -p /etc/custom;cd /etc/custom;git clone https://github.com/TheFoundation/picoinflux.git || (cd /etc/custom/picoinflux/ ; git pull ) )  || ( cd /etc/custom/picoinflux/ || ( mkdir -p /etc/custom/picoinflux/;cd /etc/custom/picoinflux/ ) ;
 wget -c "https://raw.githubusercontent.com/TheFoundation/picoinflux/master/update.sh" -O- > update.sh ;
 wget -c "https://raw.githubusercontent.com/TheFoundation/picoinflux/master/picoinflux.sh" -O- > picoinflux.sh )
 else
