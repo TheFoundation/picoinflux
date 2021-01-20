9e0df493e5beac46a975db6f371fbfd921260e21
M°ŋα Ł¹ſα <commit@only.lan>
2021-01-20 02:44:26 +0100

    2021-01-20-01.44

--- reimport.sh
+++ reimport.sh
@@ -51,7 +51,7 @@ test -f $countfile && {
     tps=$(($donecurrent/$timerans))
     [[ 0 -eq "$tps" ]] && tps=1
     togo=$(($importlength-$mywinstart))
-    eta=$(($togo/$tps/60))
+    eta=$(($togo/$tps))
     uplsize=$(tail -n+$mywinstart $importfile |head -n$windowsize|wc -c)
     echo -ne  "     queue:( $timeranm m $secrem s ) at $tps transactions/s: done $donecurrent doing  transaction (size $uplsize Byte): $mywinstart -> $mywinend  of $importlength ( "$(awk 'BEGIN {print 100*'$mywinstart'/'$importlength'}' |head -c 6 ) " % )  eta $eta  min   "'\r' >&2 ;
     tail -n+$mywinstart $importfile |head -n$windowsize |importfunction 2>&1|grep -i -e fail -e error && echo ;echo $mywinend > $countfile ;done  2>&1
