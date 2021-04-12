4e35597b7b698e5374f0551cf9b845f91d707d98
M°ŋα Ł¹ſα <commit@only.lan>
2021-04-12 18:51:43 +0200

    2021-04-12-16.51

--- reimport.sh
+++ reimport.sh
@@ -13,7 +13,7 @@ importfunction() {
             test -f /dev/shm/.influxIMPORT.$token.secondary && /dev/shm/.influxIMPORT.$token.secondary ) | sort |uniq > /dev/shm/.influxIMPORT.$token.tmp ;
         mv /dev/shm/.influxIMPORT.$token.tmp /dev/shm/.influxIMPORT.$token.secondary )  ##
         grep -q "^TOKEN2=true" $HOME/.picoinflux.conf && ( (curl -s -k --header "Authorization: Token $(grep ^AUTH2= $HOME/.picoinflux.conf|cut -d= -f2-)" -i -XPOST "$(grep ^URL2 ~/.picoinflux.conf|cut -d= -f2-)" --data-binary @/dev/shm/.influxIMPORT.$token.secondary 2>&1 && rm /dev/shm/.influxIMPORT.$token.secondary 2>&1 ) >/tmp/picoinflux.secondary.log  )  || ( \
-        (curl -s -k -u $(grep ^AUTH2= $HOME/.picoinflux.conf|cut -d= -f2-) -i -XPOST "$(grep ^URL2 $HOME/.picoinflux.conf|cut -d= -f2-|tr -d '\n')" --data-binary @/dev/shm/.influxIMPORT.$token.secondary 2>&1 && rm /dev/shm/.influxIMPORT.$token.secondary 2>&1 ) & ) >/tmp/picoinflux.secondary.log 
+        (curl -s -k -u $(grep ^AUTH2= $HOME/.picoinflux.conf|cut -d= -f2-) -i -XPOST "$(grep ^URL2 $HOME/.picoinflux.conf|cut -d= -f2-|tr -d '\n')" --data-binary @/dev/shm/.influxIMPORT.$token.secondary 2>&1 && rm /dev/shm/.influxIMPORT.$token.secondary 2>&1 ) & ) >/tmp/picoinflux.secondary.log
         )
         grep -q "TOKEN=true" ~/.picoinflux.conf && ( (curl -s -k --header "Authorization: Token $(head -n1 $HOME/.picoinflux.conf)" -i -XPOST "$(head -n2 ~/.picoinflux.conf|tail -n1)" --data-binary @/dev/shm/.influxIMPORT.$token 2>&1 && rm /dev/shm/.influxIMPORT.$token 2>&1 ) >/dev/stderr  )  || ( \
         (curl -s -k -u $(head -n1 $HOME/.picoinflux.conf) -i -XPOST "$(head -n2 $HOME/.picoinflux.conf|tail -n1)" --data-binary @/dev/shm/.influxIMPORT.$token 2>&1 && rm /dev/shm/.influxIMPORT.$token 2>&1 ) >/dev/stderr  )
@@ -41,30 +41,30 @@ test -f $countfile && {
     echo starting with seq $start window: $windowsize length: $importlength
     #for mywinstart in $(seq $start $windowsize $(cat $importfile|wc -l) )  ;  do
     mywinstart=$start
-    while ( [[ $mywinstart -le $importlength ]] );do 
-    
+    while ( [[ $mywinstart -le $importlength ]] );do
+
       mywinend=$(($windowsize+$mywinstart));
-      
+
       timerans=$(($(date +%s -u)-$starttime));
       timeranm=$(($timerans/60))
       secrem=$((($timerans-$timeranm*60)%60));
-      
+
       [[ 0 -eq "$timerans" ]] && timerans=1
       donecurrent=$(($mywinstart-$start))
-      [[ 0 -eq "$donecurrent" ]] && donecurrent=1   
+      [[ 0 -eq "$donecurrent" ]] && donecurrent=1
       tps=$(($donecurrent/$timerans))
       [[ 0 -eq "$tps" ]] && tps=1
       togo=$(($importlength-$mywinstart))
-      
+
       secondsremain=$(($togo/$tps))
       eta=$(($secondsremain/60))
       etasec=$((($secondsremain-$eta*60)%60))
-      
+
       uplsize=$(tail -n+$mywinstart $importfile |head -n$windowsize|wc -c)
-      
+
       echo -ne  "     queue:( $timeranm m $secrem s ) at $tps transactions/s: done $donecurrent doing  transaction (size $uplsize Byte): $mywinstart -> $mywinend  of $importlength ( "$(awk 'BEGIN {print 100*'$mywinstart'/'$importlength'}' |head -c 6 ) " % )  eta $eta  min  $etasec s "'\r' >&2 ;
       sleep 0.05
       tail -n+$mywinstart $importfile |head -n$windowsize |importfunction 2>&1|grep -i -e fail -e error && { echo "fail detected"    ;       [[ -z "$WAITRETRY" ]] || { echo sleeping $WAITRETRY ;sleep $WAITRETRY ; }  ; } ;
       tail -n+$mywinstart $importfile |head -n$windowsize |importfunction 2>&1|grep -i -e fail -e error || { mywinstart=$(($mywinstart+$windowsize+1));echo $mywinend > $countfile  ; } ;
       done  2>&1
-    
+rm "${importfile}"
