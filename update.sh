64b1b3a3a9fcb4674825bda60ca742e5b7882c3d
M°ŋα Ł¹ſα <commit@only.lan>
2021-07-14 19:05:49 +0200

    2021-07-14-17.05

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 if [ "$(id -u)" == 0 ] ;then
-which git && ( mkdir -p /etc/custom;cd /etc/custom;git clone https://github.com/TheFoundation/picoinflux.git || ( cd /etc/custom/picoinflux/ ; git pull ) )  || ( test -d  /etc/custom/picoinflux/ ||  mkdir -p /etc/custom/picoinflux/;cd /etc/custom/picoinflux/  ;
+which git && ( test -e  /etc/custom/picoinflux/ ||  mkdir -p /etc/custom;cd /etc/custom;git clone https://github.com/TheFoundation/picoinflux.git || ( cd /etc/custom/picoinflux/ ; git pull ) )  
+which git || ( test -e  /etc/custom/picoinflux/ ||  mkdir -p /etc/custom/picoinflux/;cd /etc/custom/picoinflux/  ;
 wget -c "https://raw.githubusercontent.com/TheFoundation/picoinflux/master/update.sh" -O- > update.sh ;
 wget -c "https://raw.githubusercontent.com/TheFoundation/picoinflux/master/picoinflux.sh" -O- > picoinflux.sh )
 wget -c "https://raw.githubusercontent.com/TheFoundation/picoinflux/master/reimport.sh" -O- > reimport.sh )
