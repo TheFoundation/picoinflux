394c6a846e505a055ca98738f7ea15df0b2646b2
M°ŋα Ł¹ſα <commit@only.lan>
2021-07-14 19:07:30 +0200

    2021-07-14-17.07

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ if [ "$(id -u)" == 0 ] ;then
 which git && ( test -e  /etc/custom/picoinflux/ ||  mkdir -p /etc/custom;cd /etc/custom;git clone https://github.com/TheFoundation/picoinflux.git || ( cd /etc/custom/picoinflux/ ; git pull ) )  
 which git || ( test -e  /etc/custom/picoinflux/ ||  mkdir -p /etc/custom/picoinflux/;cd /etc/custom/picoinflux/  ;
 wget -c "https://raw.githubusercontent.com/TheFoundation/picoinflux/master/update.sh" -O- > update.sh ;
-wget -c "https://raw.githubusercontent.com/TheFoundation/picoinflux/master/picoinflux.sh" -O- > picoinflux.sh )
+wget -c "https://raw.githubusercontent.com/TheFoundation/picoinflux/master/picoinflux.sh" -O- > picoinflux.sh ;
 wget -c "https://raw.githubusercontent.com/TheFoundation/picoinflux/master/reimport.sh" -O- > reimport.sh )
 else
 echo must be run as root
