d67ddbfbc0ada0b369afcc1c92a3d5f13ca0d8c4
resu-cireneg <69966873+resu-cireneg@users.noreply.github.com>
2021-12-17 18:52:56 +0100

    Update picoinflux.sh

--- picoinflux.sh
+++ picoinflux.sh
@@ -208,7 +208,7 @@ test -f /proc/meminfo && (cat /proc/meminfo |grep -e ^Mem -e ^VmallocTotal |sed 
 for fansp in $(find -name "fan*_input" /sys/devices/virtual/hwmon/hwmon*/ 2>/dev/null ); do echo fanspeed_$(echo  $fansp|cut -d/ -f 6)=$(cat $fansp);done
 
 
-sleep 2
+sleep 1
 
         ( ## ipv6 thread
         which ping6 >/dev/null && ( ip -6 r  s ::/0 |grep -q " metric " && echo "ping_ipv6,target=he.net"$(ping6 he.net -c 2 -w 2             2>&1|sed 's/.\+time//g' |grep ^=|sort -n|tail -n1|cut -d" " -f1|sed 's/^ \+$//g;s/^$/=-23/g'|grep -s "=" || echo "=-23" ))
@@ -216,13 +216,16 @@ sleep 2
         which ping6 >/dev/null && ( ip -6 r  s ::/0 |grep -q " metric " && echo "ping_ipv6,target=heise.de"$(ping6 heise.de -c 2 -w 2             2>&1|sed 's/.\+time//g' |grep ^=|sort -n|tail -n1|cut -d" " -f1|sed 's/^ \+$//g;s/^$/=-23/g'|grep -s "=" || echo "=-23" ))
          >&5 ) 2>>/dev/shm/picoinflux.stderr.run.log &
 ## get dockerhub counts via api
-(test -e /etc/pico.dockerhub.conf && _dockerhubstats >&5 ) 2>>/dev/shm/picoinflux.stderr.run.log  &
+(test -e /etc/pico.dockerhub.conf && _dockerhubstats |grep -v '^-' >&5 ) 2>>/dev/shm/picoinflux.stderr.run.log  &
 
-sleep 2
+sleep 1
 
 ##docker netstat
 ( docker=$(which docker) && $docker ps --format "{{.Names}}" -a|tail -n+1 |grep -v ^$| while read contline;do
                        docker container inspect $contline|grep '"NetworkMode": "host"' -q  || echo $( echo -n $contline":" ;nsenter=$(which nsenter) && ( $nsenter -t $( $docker inspect -f '{{.State.Pid}}' $(echo $contline|cut -d" " -f1)) -n sh -c "which netstat && netstat -puteen" | grep -e ^tcp -e ^udp |wc -l)  || ( $docker exec -t $contline sh -c "which netstat && netstat -puteen" |grep -e ^tcp -e ^udp|wc -l) ) ; done|sed 's/^/docker_netstat_combined,target=/g;s/:/=/g' |grep -v "=0$" >&5 ) 2>>/dev/shm/picoinflux.stderr.run.log &
+## docker traffic stats 
+( docker=$(which docker) && $docker stats --no-trunc --no-stream --all --format "table docker_net_traffic_mb\,target__EQ__{{.Name}}={{.NetIO}}"|tail -n+2|sed 's/ \/ / down \n/g;s/$/ up/g'|sed 's/=/=\n/g'|while read cont;do read down ;read up;echo $cont$down;echo $cont$up;done|sed 's/=\(.\+\) \+down$/_rx=-\1/g;s/=\(.\+\) \+up$/_tx=+\1/g;s/__EQ__/=/g'|grep -v -e '=-0B$' -e '=+0B$'|while read keyval;do key=$(echo $keyval|cut -d= -f1,2);val=${keyval/*=/};vcalc=$(echo $val|sed 's/kB/*0.001/g;s/MB/*1/g');echo -n $key=;echo|awk '{ print '$vcalc'  }' ;done >&5 ) 2>>/dev/shm/picoinflux.stderr.run.log &
+
 
 ##docker memory and cpu percent
 (
